b8388e66a7345435c823f372144f83464441a4bc
OA Wu <comdan66@gmail.com>
2015-12-18 00:03:26 +0800

    Fix read file.

--- js/about.js
+++ js/about.js
@@ -8,10 +8,12 @@ $(function () {
     $('<h2 />').text ("關於 OA's Theta S 全景相簿")).append (
     $('<div />').html ("作者名稱 - <a href='http://www.ioa.tw/' target='_blank'>OA Wu</a>")).append (
     $('<div />').html ("E-mail - <a href='mailto:comdan66@gmail.com'>comdan66@gmail.com</a>")).append (
-    $('<div />').html ("作品名稱 - <a href='http://comdan66.github.io/ThetaS/' target='_blank'>OA's ThetaS</a>")).append (
+    $('<div />').html ("作品名稱 - <a href='http://comdan66.github.io/ThetaS/' target='_blank'>OA's Theta S</a>")).append (
     $('<div />').html ("最新版本 - 1.1")).append (
-    $('<div />').html ("GitHub - <a href='https://github.com/comdan66/ThetaS' target='_blank'>OA's ThetaS</a>")).append (
-    $('<div />').html ("更新日期 - 2015/12/17")).prependTo (window.container);
+    $('<div />').html ("GitHub - <a href='https://github.com/comdan66/ThetaS' target='_blank'>OA's Theta S</a>")).append (
+    $('<div />').html ("ThetaS - <a href='https://theta360.com/ct/about/theta/s.html' target='_blank'>官方網站</a>")).append (
+    $('<div />').html ("相關資源 - <a href='https://github.com/shokai/theta-viewer.js' target='_blank'>theta-viewer.js</a>")).append (
+    $('<div />').html ("更新日期 - 2015/12/18")).prependTo (window.container);
 
   window.closeLoading ();
 });
--- readme.md
+++ readme.md
@@ -1,14 +1,27 @@
-# Welcome To OA's F2E Framework!
-OA 個人常用的前端工具總集合！ 
+# Welcome To OA's Theta S!
+搭配 Ricoh Theta S 記錄環景、全景、360度、小星球(Stereographics Projection) 的一個 OA's 個人全景相簿！
 
 ---
+<br/>
+## 範例
+Demo: [http://comdan66.github.io/ThetaS/index.html](http://comdan66.github.io/ThetaS/index.html)
+
+<br/>
 ## 聲明
 本作品授權採用 姓名標示-非商業性 2.0 台灣 (CC BY-NC 2.0 TW) 授權，詳見 [http://creativecommons.org/licenses/by-nc/2.0/tw/](http://creativecommons.org/licenses/by-nc/2.0/tw/)
 
 
 <br/>
 ## 簡介
-* 個人習慣工具，有興趣的參考吧！
+* 純粹因為買了一台 Ricoh Theta S，就想說來寫一個 360度 全視角的相簿！
+* 搭配 Ricoh Theta S 記錄環景、全景、360度、小星球(Stereographics Projection) 的個人全景相簿！
+
+<br/>
+## RICOH THETA S
+* ㄜ.. 他就是 [RICOH THETA S](https://theta360.com/ct/)  
+￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬ ￬   
+
+[![OA's Theta S 全景相簿](img/readme/ricoh.jpg)](https://theta360.com/ct/)
 
 <br/>
 ## 關於
@@ -16,10 +29,13 @@ OA 個人常用的前端工具總集合！
 
 * E-mail - <comdan66@gmail.com>
 
-* 作品名稱 - OA's F2E Framework
+* 作品名稱 - OA's Theta S
 
-* 最新版本 - 1.2
+* 最新版本 - 1.1
 
-* GitHub - [OA's F2E Framework](https://github.com/comdan66/oaf2e/)
+* GitHub - [OA's Theta S](https://github.com/comdan66/ThetaS)
 
-* 更新日期 - 2015/05/26
+* theta-viewer.js - [theta-viewer.js](https://github.com/shokai/theta-viewer.js)
+
+
+* 更新日期 - 2015/12/18
